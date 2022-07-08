c8409efed9a10aef1ce382c03484c83c57f3b333
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-07-08 16:20:54 +0100

    Light and dark theme

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -6,4 +6,14 @@
 // ** Imported  **
 // *==============================================================================
 
-console.log('Hello');
+const toggleThemeButton = document.querySelector('.toggle-theme-button');
+const root = document.documentElement;
+const themeText = document.querySelector('.current-theme-text');
+
+function toggleTheme() {
+  const switchClass = root.className === 'light' ? 'dark' : 'light';
+  root.className = switchClass;
+  themeText.innerText = switchClass;
+}
+
+toggleThemeButton.addEventListener('click', toggleTheme);
--- scripts/minified/main-min.js
+++ scripts/minified/main-min.js
@@ -1,2 +1,2 @@
-console.log("Hello");
+!function(){const e=document.querySelector(".toggle-theme-button"),t=document.documentElement,n=document.querySelector(".current-theme-text");e.addEventListener("click",(function(){const e="light"===t.className?"dark":"light";t.className=e,n.innerText=e}))}();
 //# sourceMappingURL=main-min.js.map
